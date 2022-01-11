05a0e109336c6f11bf829f47fb25095867bd3e8f
Duck Splash <11778864+ducksplash@users.noreply.github.com>
2022-01-11 13:37:29 +0000

    Update integrated_token_presale_and_airdrop.sol
    code update

--- integrated_token_presale_and_airdrop.sol
+++ integrated_token_presale_and_airdrop.sol
@@ -1,7 +1,12 @@
 /*
+
 Integrated Token, Presale & Airdrop contract.
 
+
+
+
 Pertinent Variables: 
+
 
    Fill these in below (in the constructor, not this comment!)
 
@@ -31,6 +36,14 @@ Pertinent Variables:
 
         // presale balance, 5m  
         presaleBalance = 5000000000000000000000000; 
+
+
+
+
+
+
+
+
 */
 pragma solidity ^0.4.26;
 
